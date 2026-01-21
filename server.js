baa6415a0f3f1279ccfaf0201a06b7a8c32f0948
maqsudamadrahimova.9@gmail.com <maqsudamadrahimova.9@gmail.com>
2026-01-21 12:07:22 +0500

    4-oy-2-dars misollar qo'shildi

--- server.js
+++ server.js
@@ -1,3 +1,72 @@
-function ha(m){
-    
-}
+// import {add, substract}  from "./math.js";
+
+// console.log(add(15,3));
+// console.log(substract(15,3));
+
+
+
+// require("./math")
+
+
+
+// const { error } = require("console")
+// const fs = require("fs")
+// fs.writeFileSync("main.txt", "Salom, dunyo!")
+// console.log("yaratdim");
+
+// let data = fs.readFileSync("main.txt", "utf-8")
+// console.log(data);
+
+
+
+// const nums = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100))
+// const buf = Buffer.from(JSON.stringify(nums))
+// fs.writeFileSync("data.bin", buf)
+
+// const readBuf = fs.readFileSync("data.bin")
+// console.log(readBuf);
+
+// const result = JSON.parse(readBuf.toString())
+// console.log(result);
+
+
+
+
+
+
+//                 3-topshiriq
+
+// fs.mkdir("html",(error) => {
+//     if(error) console.log(error);
+// })
+
+// fs.appendFileSync("html/index.html","salom")
+
+
+// fs.mkdir("static",(error) => {
+//     if(error) console.log(error);
+// })
+
+// fs.appendFileSync("static/script.js","salom")
+// fs.appendFileSync("static/script.ts","salom")
+// fs.appendFileSync("static/style.css","salom")
+
+// fs.appendFileSync("go.mod","salom")
+
+// fs.appendFileSync("go.sum","g")  
+
+// fs.appendFileSync("golang-todo-app","hi")
+
+// fs.appendFileSync("main.go","hi")
+
+// fs.appendFileSync("README.md","hi")
+
+
+
+// export function add(a,b) {
+//     return a+b
+// }
+
+// export function substract(a,b) {
+//     return a-b
+// }
